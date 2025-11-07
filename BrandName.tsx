bb88900984baf059749693f8d45fafe3252ef7c2
comeman98-ux <comeman98@gmail.com>
2025-11-07 12:47:35 +0800

    Add files via upload

---
+++ BrandName.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useLanguage } from '@/contexts/LanguageContext';
+
+export default function BrandName() {
+  const { language } = useLanguage();
+
+  if (language === 'zh') {
+    return (
+      <>
+        <span className="font-black">汇</span>
+        <span className="font-normal text-gray-600 dark:text-gray-400">刃</span>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <span className="font-black">FX</span>
+      <span className="font-normal text-gray-600 dark:text-gray-400">Killer</span>
+    </>
+  );
+}
